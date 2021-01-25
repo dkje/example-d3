2d50f67c22f2087391f0aa3e16a6e7830b2b7cb1
dkje <developer.kje@gmail.com>
2021-01-25 16:29:21 +0900

    default chart done

--- bar-chart/src/D3Chart.js
+++ bar-chart/src/D3Chart.js
@@ -1,31 +1,75 @@
 import * as d3 from "d3";
 
 const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
+const MARGIN = { TOP: 10, BOTTOM: 60, LEFT: 70, RIGHT: 10 };
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
   constructor(element) {
     this.svg = d3
       .select(element)
       .append("svg")
-      .attr("width", 800)
-      .attr("height", 500);
+      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+      .append("g")
+      .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
     this.init();
   }
 
   async init() {
+    // get remote data
     const data = await d3.json(url);
-    console.log(data);
+
+    // create height, width
+    const max = d3.max(data, (d) => d.height); // 가장 큰 키를 반환한다
+    const min = d3.min(data, (d) => d.height);
+    const y = d3
+      .scaleLinear()
+      .domain([min * 0.95, max * 1.01])
+      .range([HEIGHT, 0]); // 위로 갈수록 높은 값이 되도록 세팅
+    const x = d3
+      .scaleBand()
+      .domain(data.map((d) => d.name))
+      .range([0, WIDTH])
+      .padding(0.4);
+
+    //create Axis
+    const xAxisCall = d3.axisBottom(x);
+    this.svg
+      .append("g")
+      .attr("transform", `translate(0,${HEIGHT})`)
+      .call(xAxisCall);
+
+    const yAxisCall = d3.axisLeft(y);
+    this.svg.append("g").call(yAxisCall);
+
+    // apeend title
+    this.svg
+      .append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", HEIGHT + 50)
+      .attr("text-anchor", "middle")
+      .text("The world's tallest men");
+
+    this.svg
+      .append("text")
+      .attr("x", -(HEIGHT / 2))
+      .attr("y", -50)
+      .attr("text-anchor", "middle")
+      .text("Height in cm")
+      .attr("transform", "rotate(-90)");
 
     const rects = this.svg.selectAll("rect").data(data);
 
     rects
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 100)
-      .attr("y", 0)
-      .attr("width", 50)
-      .attr("height", (d) => (d.height - 230) * 10)
+      .attr("x", (d, i) => x(d.name))
+      .attr("y", (d) => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", (d) => HEIGHT - y(d.height)) // y range가 역순이 되었기 때문에 재 계산
       .attr("fill", "grey");
   }
 }
