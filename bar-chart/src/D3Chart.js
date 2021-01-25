fbfd30c15f920a135e5abd59ce06d8d72f4759ca
dkje <developer.kje@gmail.com>
2021-01-25 17:01:44 +0900

    update method added & move render logic to update method

--- bar-chart/src/D3Chart.js
+++ bar-chart/src/D3Chart.js
@@ -7,7 +7,8 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
   constructor(element) {
-    this.svg = d3
+    const vis = this;
+    vis.svg = d3
       .select(element)
       .append("svg")
       .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -15,45 +16,15 @@ export default class D3Chart {
       .append("g")
       .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
-    this.init();
-  }
-
-  async init() {
-    // get remote data
-    const data = await d3.json(url);
-
-    // create height, width
-    const max = d3.max(data, (d) => d.height); // 가장 큰 키를 반환한다
-    const min = d3.min(data, (d) => d.height);
-    const y = d3
-      .scaleLinear()
-      .domain([min * 0.95, max * 1.01])
-      .range([HEIGHT, 0]); // 위로 갈수록 높은 값이 되도록 세팅
-    const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.name))
-      .range([0, WIDTH])
-      .padding(0.4);
-
-    //create Axis
-    const xAxisCall = d3.axisBottom(x);
-    this.svg
-      .append("g")
-      .attr("transform", `translate(0,${HEIGHT})`)
-      .call(xAxisCall);
-
-    const yAxisCall = d3.axisLeft(y);
-    this.svg.append("g").call(yAxisCall);
-
-    // apeend title
-    this.svg
+    // apeend labels
+    vis.svg
       .append("text")
       .attr("x", WIDTH / 2)
       .attr("y", HEIGHT + 50)
       .attr("text-anchor", "middle")
       .text("The world's tallest men");
 
-    this.svg
+    vis.svg
       .append("text")
       .attr("x", -(HEIGHT / 2))
       .attr("y", -50)
@@ -61,15 +32,52 @@ export default class D3Chart {
       .text("Height in cm")
       .attr("transform", "rotate(-90)");
 
-    const rects = this.svg.selectAll("rect").data(data);
+    vis.xAxisGroup = vis.svg
+      .append("g")
+      .attr("transform", `translate(0,${HEIGHT})`);
 
-    rects
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => x(d.name))
-      .attr("y", (d) => y(d.height))
-      .attr("width", x.bandwidth)
-      .attr("height", (d) => HEIGHT - y(d.height)) // y range가 역순이 되었기 때문에 재 계산
-      .attr("fill", "grey");
+    vis.yAxisGroup = vis.svg.append("g");
+
+    // get remote data
+    d3.json(url).then((data) => {
+      vis.data = data;
+      d3.interval(() => {
+        vis.update();
+      }, 1000);
+    });
+  }
+
+  update() {
+    const vis = this;
+    // scale
+    const max = d3.max(vis.data, (d) => d.height); // 가장 큰 키를 반환한다
+    const min = d3.min(vis.data, (d) => d.height);
+    const y = d3
+      .scaleLinear()
+      .domain([min * 0.95, max * 1.01])
+      .range([HEIGHT, 0]); // 위로 갈수록 높은 값이 되도록 세팅
+    const x = d3
+      .scaleBand()
+      .domain(vis.data.map((d) => d.name))
+      .range([0, WIDTH])
+      .padding(0.4);
+
+    //create Axis
+    const xAxisCall = d3.axisBottom(x);
+    vis.xAxisGroup.attr("transform", `translate(0,${HEIGHT})`).call(xAxisCall);
+
+    const yAxisCall = d3.axisLeft(y);
+    vis.yAxisGroup.call(yAxisCall);
+
+    // const rects = vis.svg.selectAll("rect").data(data);
+
+    // rects
+    //   .enter()
+    //   .append("rect")
+    //   .attr("x", (d, i) => x(d.name))
+    //   .attr("y", (d) => y(d.height))
+    //   .attr("width", x.bandwidth)
+    //   .attr("height", (d) => HEIGHT - y(d.height)) // y range가 역순이 되었기 때문에 재 계산
+    //   .attr("fill", "grey");
   }
 }
